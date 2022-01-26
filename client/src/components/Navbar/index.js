2f64c3f266709c5bd4c0ffb0d635dd00280ed8c6
elliott dombrowski <elliottroyal@me.com>
2022-01-25 23:29:37 -0600

    added links to navbar, added basic positioning styles

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 import './query.css';
@@ -7,15 +8,39 @@ const Navbar = () => {
   return (
     <div className='navbar-wrapper'>
       <nav className='nav-left'>
-        left
+        <ul className='nav-item-wrapper'>
+          <li className='nav-item'>
+            <Link to='/'>
+              news
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/'>
+              blog
+            </Link>
+          </li>
+        </ul>
       </nav>
 
       <nav className='nav-center'>
-        center
+        <Link to='/'>
+          center
+        </Link>
       </nav>
 
       <nav className='nav-right'>
-        right 
+        <ul className='nav-item-wrapper'>
+          <li className='nav-item'>
+            <Link to='/'>
+              charities
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/'>
+              log in
+            </Link>
+          </li>
+        </ul>
       </nav>
     </div>
   );
