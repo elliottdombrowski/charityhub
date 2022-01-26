0d127801bc08092658ac44b234b4c1b41fff411c
elliott dombrowski <elliottroyal@me.com>
2022-01-25 23:08:18 -0600

    added navbar component

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+import './styles.css';
+import './query.css';
+
+const Navbar = () => {
+  return (
+    <div className='navbar-wrapper'>
+
+    </div>
+  );
+}
+
+export default Navbar;
