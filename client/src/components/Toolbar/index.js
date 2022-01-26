3aca0d2fd74edf7025e0e0d2a3aac011059e24c5
elliott dombrowski <elliottroyal@me.com>
2022-01-25 23:49:47 -0600

    added luxon date package formatted to M/DD/YYYY/LOCALTIME

--- client/src/components/Toolbar/index.js
+++ client/src/components/Toolbar/index.js
@@ -1,14 +1,36 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+
 import './styles.css';
 import './query.css';
 
+//IMPORT LUXON DATE PACKAGE
+const { DateTime } = require('luxon');
+
 const Toolbar = () => {
-  return(
+  //GET CURRENT TIME - CURRENT HARDCODED TO CHICAGO
+  let getTime = DateTime.now().toFormat("DD',' t");
+
+  return (
     <div className='toolbar-wrapper'>
-      <p>toolbar</p>
-    </div>
+      <nav className='toolbar-left'>
+        <ul className='nav-item-wrapper'>
+          <li className='nav-item'>
+            {getTime}
+          </li>
+          <li className='nav-item'>
+            <Link to='/'>
+              blog
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <nav className='toolbar-right'>
+
+      </nav>
+    </div >
   );
 };
 
