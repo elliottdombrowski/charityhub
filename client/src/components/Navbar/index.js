e4d29ddaa6aaffb0332953745f29d4dde42b20d3
elliott dombrowski <elliottroyal@me.com>
2022-01-25 23:15:01 -0600

    added css root vars

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,7 +6,17 @@ import './query.css';
 const Navbar = () => {
   return (
     <div className='navbar-wrapper'>
+      <nav className='nav-left'>
+        left
+      </nav>
 
+      <nav className='nav-center'>
+        center
+      </nav>
+
+      <nav className='nav-right'>
+        right 
+      </nav>
     </div>
   );
 }
