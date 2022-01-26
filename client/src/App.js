8275010a77c23f523d5bc16439b748cd80d5ed1e
elliott dombrowski <elliottroyal@me.com>
2022-01-25 23:03:31 -0600

    added chakra provider to get css unsets out of the way

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>testing</h1>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <h1>testing</h1>
+      </div>
+    </ChakraProvider>
   );
 }
 
