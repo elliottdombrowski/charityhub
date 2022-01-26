84f2961c2159e6ec1a304d42e724f47b4a067825
elliott dombrowski <elliottroyal@me.com>
2022-01-25 22:54:34 -0600

    configured server enough for heroku deployment

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+};
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
