6d8469b4aec183fa3c0c65ebbb02732b10c9a2f8
elliott dombrowski <elliottroyal@me.com>
2022-01-25 23:06:36 -0600

    added filestructure for pages and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react';
+
+import Navbar from './components/Navbar/index.js';
+
 import './App.css';
 
 function App() {
   return (
     <ChakraProvider>
       <div className="App">
-        <h1>testing</h1>
+        <Router>
+          <Navbar />
+        </Router>
       </div>
     </ChakraProvider>
   );
