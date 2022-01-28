8a5e75a20f8dc30d89d2cf0e3727911df0eaeed9
elliott dombrowski <elliottroyal@me.com>
2022-01-27 23:34:01 -0600

    started login form

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -5,6 +5,10 @@ import './styles.css';
 import './query.css';
 
 const LoginForm = () => {
+  const [showPwd, setShowPwd] = useState(false);
+  let inputType = input.type;
+  showPwd ? inputType = 'text' : 'password';
+
   const loginSubmit = () => {
     console.log('logging in');
   };
@@ -15,18 +19,35 @@ const LoginForm = () => {
         onSubmit={loginSubmit}
         className="login-form"
       >
+        <label className="login-label">
+          log in
+        </label>
         <Input 
           variant='filled'
           value='email'
           type='text'
           className='login-input'
         />
-        <Input 
-          variant='filled'
-          value='password'
-          type='text'
-          className='login-input'
-        />
+        <div className="password-wrapper">
+          <Input 
+            variant='filled'
+            value='password'
+            type={showPwd}
+            className='login-input'
+          />
+          <button className="show-password">
+            show
+          </button>
+        </div>
+        <span className="save-login">
+          <label className="login-label">
+            keep me logged in
+          </label>
+          <input
+            className="save-login-checkbox"
+            type='checkbox'
+          />
+        </span>
       </form>
     </div>
   );
