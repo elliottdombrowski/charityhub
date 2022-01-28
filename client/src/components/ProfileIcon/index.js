3f9bf6d0c71864661f5e0b66ae9f2a678f6b432a
elliott dombrowski <elliottroyal@me.com>
2022-01-27 19:29:52 -0600

    oriented toolbar

--- client/src/components/ProfileIcon/index.js
+++ client/src/components/ProfileIcon/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
-const profile = <FontAwesomeIcon icon={faUserCircle} className='fa-2x' />
+const profile = <FontAwesomeIcon icon={faUserCircle} className='fa-lg' />
 
 const ProfileIcon = () => {
   return (
     <li className='nav-item'>
-      {profile}
+      <Link to='/profile'>
+        {profile}
+      </Link>
     </li>
   );
 }
