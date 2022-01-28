fa10f38b466238ffeaf165515cdce3226cded6d8
elliott dombrowski <elliottroyal@me.com>
2022-01-27 23:12:06 -0600

    added default render for loginform

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ function App() {
           <Route exact path='/'>
             <Navbar />
             <Toolbar />
+            <LoginForm />
           </Route>
         </Router>
       </div>
---
+++ client/src/components/LoginForm/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+import './styles.css';
+import './query.css';
+
+const LoginForm = () => {
+  return (
+    <div className="login-form-wrapper">
+      
+    </div>
+  );
+};
+
+export default LoginForm;
