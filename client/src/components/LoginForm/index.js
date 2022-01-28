879dd4e2a8b9aabdad424bd691eec9c737a50b74
elliott dombrowski <elliottroyal@me.com>
2022-01-27 23:19:48 -0600

    added starter display for login form to help visuals

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,12 +1,33 @@
 import React, { useState } from "react";
+import { Input } from "@chakra-ui/react";
 
 import './styles.css';
 import './query.css';
 
 const LoginForm = () => {
+  const loginSubmit = () => {
+    console.log('logging in');
+  };
+
   return (
     <div className="login-form-wrapper">
-      
+      <form 
+        onSubmit={loginSubmit}
+        className="login-form"
+      >
+        <Input 
+          variant='filled'
+          value='email'
+          type='text'
+          className='login-input'
+        />
+        <Input 
+          variant='filled'
+          value='password'
+          type='text'
+          className='login-input'
+        />
+      </form>
     </div>
   );
 };
