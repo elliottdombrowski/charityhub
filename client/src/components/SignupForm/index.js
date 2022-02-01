bbc5d09e8e236539d6f78a14cee4ef79984ddec1
elliott dombrowski <elliottroyal@me.com>
2022-02-01 17:28:37 -0600

    added starter buttons for location services

--- client/src/components/SignupForm/index.js
+++ client/src/components/SignupForm/index.js
@@ -19,6 +19,7 @@ const SignupForm = () => {
   const [showPwd, setShowPwd] = useState(false);
   const [showLocationInfo, setShowLocationInfo] = useState(false);
   const [showLocationForm, setShowLocationForm] = useState(false);
+  const [useLocationServices, setUseLocationServices] = useState(true);
 
   const handleShowLocationInfo = () => {
     setShowLocationInfo(!showLocationInfo);
@@ -103,106 +104,134 @@ const SignupForm = () => {
       <div className='signup-right' id='location-form'>
         <label className='login-label'>
           Location
+          <span className='required-label'>*</span>
         </label>
         <span className='location-signup-wrapper'>
-          <div className='location-wrapper-left'>
-            <Input
-              variant='filled'
-              type='text'
-              name='city'
-              onChange={handleInputChange}
-              value={signupData.city}
-              placeholder='city'
-              className='login-input login-input-city'
-            />
-          </div>
+          {useLocationServices ? (
+            <div className='location-wrapper-right location-services-wrapper'>
+              <button
+                type='text'
+                className='location-services-optional location-services-button'
+              >
+                Learn More
+              </button>
 
-          <div className='location-wrapper-right'>
-            <Select
-              variant='filled'
-              type='text'
-              name='state'
-              onChange={handleInputChange}
-              value={signupData.state}
-              placeholder='state'
-              className='login-input login-input-state'
-            >
-              <option value="AL">Alabama</option>
-              <option value="AK">Alaska</option>
-              <option value="AZ">Arizona</option>
-              <option value="AR">Arkansas</option>
-              <option value="CA">California</option>
-              <option value="CO">Colorado</option>
-              <option value="CT">Connecticut</option>
-              <option value="DE">Delaware</option>
-              <option value="DC">District of Columbia</option>
-              <option value="FL">Florida</option>
-              <option value="GA">Georgia</option>
-              <option value="HI">Hawaii</option>
-              <option value="ID">Idaho</option>
-              <option value="IL">Illinois</option>
-              <option value="IN">Indiana</option>
-              <option value="IA">Iowa</option>
-              <option value="KS">Kansas</option>
-              <option value="KY">Kentucky</option>
-              <option value="LA">Louisiana</option>
-              <option value="ME">Maine</option>
-              <option value="MD">Maryland</option>
-              <option value="MA">Massachusetts</option>
-              <option value="MI">Michigan</option>
-              <option value="MN">Minnesota</option>
-              <option value="MS">Mississippi</option>
-              <option value="MO">Missouri</option>
-              <option value="MT">Montana</option>
-              <option value="NE">Nebraska</option>
-              <option value="NV">Nevada</option>
-              <option value="NH">New Hampshire</option>
-              <option value="NJ">New Jersey</option>
-              <option value="NM">New Mexico</option>
-              <option value="NY">New York</option>
-              <option value="NC">North Carolina</option>
-              <option value="ND">North Dakota</option>
-              <option value="OH">Ohio</option>
-              <option value="OK">Oklahoma</option>
-              <option value="OR">Oregon</option>
-              <option value="PA">Pennsylvania</option>
-              <option value="RI">Rhode Island</option>
-              <option value="SC">South Carolina</option>
-              <option value="SD">South Dakota</option>
-              <option value="TN">Tennessee</option>
-              <option value="TX">Texas</option>
-              <option value="UT">Utah</option>
-              <option value="VT">Vermont</option>
-              <option value="VA">Virginia</option>
-              <option value="WA">Washington</option>
-              <option value="WV">West Virginia</option>
-              <option value="WI">Wisconsin</option>
-              <option value="WY">Wyoming</option>
-            </Select>
-          </div>
+              <button
+                type='text'
+                className='location-services-secondary location-services-button'
+                onClick={(prev) => setUseLocationServices((prev) => !prev)}
+              >
+                Don't Allow
+              </button>
+
+              <button
+                type='text'
+                className='location-services-primary location-services-button'
+              >
+                Allow
+              </button>
+            </div>
+          ) : (
+            <>
+              <div className='location-wrapper-left'>
+                <Input
+                  variant='filled'
+                  type='text'
+                  name='city'
+                  onChange={handleInputChange}
+                  value={signupData.city}
+                  placeholder='city'
+                  className='login-input login-input-city'
+                />
+              </div>
+              <div className='location-wrapper-right'>
+                <Select
+                  variant='filled'
+                  type='text'
+                  name='state'
+                  onChange={handleInputChange}
+                  value={signupData.state}
+                  placeholder='state'
+                  className='login-input login-input-state'
+                >
+                  <option value="AL">Alabama</option>
+                  <option value="AK">Alaska</option>
+                  <option value="AZ">Arizona</option>
+                  <option value="AR">Arkansas</option>
+                  <option value="CA">California</option>
+                  <option value="CO">Colorado</option>
+                  <option value="CT">Connecticut</option>
+                  <option value="DE">Delaware</option>
+                  <option value="DC">District of Columbia</option>
+                  <option value="FL">Florida</option>
+                  <option value="GA">Georgia</option>
+                  <option value="HI">Hawaii</option>
+                  <option value="ID">Idaho</option>
+                  <option value="IL">Illinois</option>
+                  <option value="IN">Indiana</option>
+                  <option value="IA">Iowa</option>
+                  <option value="KS">Kansas</option>
+                  <option value="KY">Kentucky</option>
+                  <option value="LA">Louisiana</option>
+                  <option value="ME">Maine</option>
+                  <option value="MD">Maryland</option>
+                  <option value="MA">Massachusetts</option>
+                  <option value="MI">Michigan</option>
+                  <option value="MN">Minnesota</option>
+                  <option value="MS">Mississippi</option>
+                  <option value="MO">Missouri</option>
+                  <option value="MT">Montana</option>
+                  <option value="NE">Nebraska</option>
+                  <option value="NV">Nevada</option>
+                  <option value="NH">New Hampshire</option>
+                  <option value="NJ">New Jersey</option>
+                  <option value="NM">New Mexico</option>
+                  <option value="NY">New York</option>
+                  <option value="NC">North Carolina</option>
+                  <option value="ND">North Dakota</option>
+                  <option value="OH">Ohio</option>
+                  <option value="OK">Oklahoma</option>
+                  <option value="OR">Oregon</option>
+                  <option value="PA">Pennsylvania</option>
+                  <option value="RI">Rhode Island</option>
+                  <option value="SC">South Carolina</option>
+                  <option value="SD">South Dakota</option>
+                  <option value="TN">Tennessee</option>
+                  <option value="TX">Texas</option>
+                  <option value="UT">Utah</option>
+                  <option value="VT">Vermont</option>
+                  <option value="VA">Virginia</option>
+                  <option value="WA">Washington</option>
+                  <option value="WV">West Virginia</option>
+                  <option value="WI">Wisconsin</option>
+                  <option value="WY">Wyoming</option>
+                </Select>
+              </div>
+              <div className='signup-location-wrapper'>
+                <span
+                  className='signup-location-header'
+                  onClick={handleShowLocationInfo}
+                >
+                  How do we use this information?
+                  {arrowDown}
+                </span>
+                <div
+                  className='signup-location-info'
+                  id='location-info'
+                >
+                  <p
+                    className='signup-location-text'
+                    id='location-text'
+                  >
+                    We collect this information solely to provide information relevant to you.
+                    This information will never be distributed, sold, or misused.
+                  </p>
+                </div>
+              </div>
+            </>
+          )}
         </span>
-        <div className='signup-location-wrapper'>
-          <span
-            className='signup-location-header'
-            onClick={handleShowLocationInfo}
-          >
-            How do we use this information?
-            {arrowDown}
-          </span>
-          <div
-            className='signup-location-info'
-            id='location-info'
-          >
-            <p
-              className='signup-location-text'
-              id='location-text'
-            >
-              We collect this information solely to provide information relevant to you.
-              This information will never be distributed, sold, or misused.
-            </p>
-          </div>
-        </div>
-      </div>
+      </div >
 
       <div className='login-options'>
         <span className="save-login">
