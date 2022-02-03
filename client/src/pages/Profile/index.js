5e7032de5cf3ee4fa732c510ec0cf262fc8a84fb
elliott dombrowski <elliottroyal@me.com>
2022-02-02 19:42:05 -0600

    added blank profile img to profile page

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -6,7 +6,24 @@ import './query.css';
 const Profile = () => {
   return (
     <main className='profile-wrapper'>
-      
+      <section className='profile-body-left'>
+        <div className='profile-img-wrapper'>
+          <img
+            src={require('../../assets/blank-profile.png')}
+            alt='user profile photo'
+            className='profile-img'
+          ></img>
+        </div>
+      </section>
+
+      <section className='profile-body-right'>
+
+      </section>
+      {/* <input 
+        type='file'
+        name='profile-image'
+        accept='image/*'
+      /> */}
     </main>
   );
 };
