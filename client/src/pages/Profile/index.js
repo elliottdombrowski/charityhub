97d820fd3b4dcf898dbdf3351bcf935e0d8f738d
elliott dombrowski <elliottroyal@me.com>
2022-02-02 19:24:38 -0600

    created starting render for profile page

---
+++ client/src/pages/Profile/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+import './styles.css';
+import './query.css';
+
+const Profile = () => {
+  return (
+    <main className='profile-wrapper'>
+      
+    </main>
+  );
+};
+
+export default Profile;
