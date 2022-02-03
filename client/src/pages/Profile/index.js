f9b777aa1445ca5166ce05f8bbfa0b57fabbdfb1
elliott dombrowski <elliottroyal@me.com>
2022-02-02 20:18:14 -0600

    added boolean toggler on settings icon

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -4,17 +4,33 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_ME } from '../../utils/queries';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+
 import './styles.css';
 import './query.css';
 
-const Profile = () => {
-  const { name: userParam } = useParams();
 
+const Profile = () => {
+  const [showSettings, setShowSettings] = useState(false);
+  
+  const { name: userParam } = useParams();
+  
   const { loading, data } = useQuery(QUERY_ME, {
     variables: { username: userParam },
   });
-
+  
   const user = data?.me || [];
+
+  const displaySettings = () => {
+    console.log('displaying settings');
+    setShowSettings(!showSettings);
+    showSettings
+      ? document.getElementById('settings-display').classList.add('active')
+      : document.getElementById('settings-display').classList.remove('active')
+  };
+
+  const angle = <FontAwesomeIcon className='settings-icon' id='settings-display' icon={faAngleDown} onClick={displaySettings}/>
 
   return (
     <main className='profile-wrapper'>
@@ -36,13 +52,25 @@ const Profile = () => {
           <h2 className='profile-location profile-data'>{user.city}, {user.state}</h2>
         </section>
 
-        <span className='profile-settings'>
-          {/* PLACEHOLDERS FOR NOW  */}
-          <p>dark mode</p>
-          <p>change password</p>
-          <p>change picture</p>
-          <p>change location</p>
-          <p>disable location</p>
+        <span className='profile-settings-wrapper'>
+          <h3 className='profile-settings-header'>
+            Settings
+          </h3>
+          <ul className='profile-settings'>
+            <li className='profile-option'>
+              dark mode
+            </li>
+            <li className='profile-option'>
+              {angle}
+              user settings
+            </li>
+            <li className='profile-option'>
+              {angle}
+              location settings
+            </li>
+            <li className='profile-option'></li>
+            <li className='profile-option'></li>
+          </ul>
         </span>
       </section>
 
