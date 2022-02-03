71fe5bb4c0459adaad6b3b3e14c5a2b63c1ef99d
elliott dombrowski <elliottroyal@me.com>
2022-02-02 19:58:13 -0600

    added placeholder text for profile settings for now

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { QUERY_ME } from '../../utils/queries'; 
+import { QUERY_ME } from '../../utils/queries';
 
 import './styles.css';
 import './query.css';
@@ -27,12 +27,23 @@ const Profile = () => {
           ></img>
         </div>
 
-        <section className='profile-info'>
+        <div className='profile-name'>
           <h1>{user.name}</h1>
-          <h2>{user.email}</h2>
-          <h2>{user.city}</h2>
-          <h2>{user.state}</h2>
+        </div>
+
+        <section className='profile-info'>
+          <h2 className='profile-email profile-data'>{user.email}</h2>
+          <h2 className='profile-location profile-data'>{user.city}, {user.state}</h2>
         </section>
+
+        <span className='profile-settings'>
+          {/* PLACEHOLDERS FOR NOW  */}
+          <p>dark mode</p>
+          <p>change password</p>
+          <p>change picture</p>
+          <p>change location</p>
+          <p>disable location</p>
+        </span>
       </section>
 
       <section className='profile-body-right'>
