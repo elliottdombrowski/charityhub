689233ea53b0e4a33b395af06a358825106e74f4
elliott dombrowski <elliottroyal@me.com>
2022-02-02 19:20:43 -0600

    added ternary rendering for profile link to redirect to login if not authorized

--- client/src/components/ProfileIcon/index.js
+++ client/src/components/ProfileIcon/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
 const profile = <FontAwesomeIcon icon={faUserCircle} className='fa-lg' />
 
 const ProfileIcon = () => {
   return (
     <li className='nav-item'>
-      <Link to='/profile'>
+      <Link to={Auth.loggedIn() ? '/profile' : '/login'}>
         {profile}
       </Link>
     </li>
