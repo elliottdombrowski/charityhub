a77a5a9202f2a12a58cf8b4b7f8f017acc923739
elliott dombrowski <elliottroyal@me.com>
2022-02-06 15:25:34 -0600

    added donation btn to card

--- client/src/components/DonationCard/index.js
+++ client/src/components/DonationCard/index.js
@@ -6,7 +6,13 @@ import './query.css';
 const DonationCard = () => {
   return (
     <div className="donation-card">
-      
+      <h1 className="card-header">
+        DUMMY NAME
+      </h1>
+
+      <button className='donate-btn'>
+        test
+      </button>
     </div>
   );
 };
