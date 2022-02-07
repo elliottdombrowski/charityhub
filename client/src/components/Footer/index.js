c83bc8fb164722cb77061c1a30104ee787f0dd04
elliott dombrowski <elliottroyal@me.com>
2022-02-07 15:21:52 -0600

    added starter footer render

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css';
+import './query.css';
+
+const Footer = () => {
+  return (
+    <footer className='footer-wrapper'>
+      <p>footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
