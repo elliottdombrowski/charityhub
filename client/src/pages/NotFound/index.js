43f5a4d6c146a623f381816247d6d9e92b9690cd
elliott dombrowski <elliottroyal@me.com>
2022-02-08 12:59:43 -0600

    added 404 subheader

--- client/src/pages/NotFound/index.js
+++ client/src/pages/NotFound/index.js
@@ -7,10 +7,17 @@ import './query.css';
 const NotFound = () => {
   return (
     <main className='notfound-wrapper'>
-      <h1 className='notfound-header'>404!</h1>
+      <h1 className='notfound-header'>
+        404!
+      </h1>
+      
       <div className='notfound-svg-wrapper'>
         <NotFoundSvg />
       </div>
+
+      <h2 className='notfound-subheader'>
+        Whoops! The page you're looking for can't be found.
+      </h2>
     </main>
   );
 };
