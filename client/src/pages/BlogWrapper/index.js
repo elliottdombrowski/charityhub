1b15aacb92d5dc435b255c108ea490ee40303b7e
elliott dombrowski <elliottroyal@me.com>
2022-02-09 16:08:56 -0600

    added starter blog search bar andn starter hover/focus statse

--- client/src/pages/BlogWrapper/index.js
+++ client/src/pages/BlogWrapper/index.js
@@ -2,13 +2,34 @@ import React from 'react';
 import BlogForm from '../../components/BlogForm';
 import BlogPost from '../../components/BlogPost';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 import './styles.css';
 import './query.css';
+
+const searchIcon = <FontAwesomeIcon icon={faSearch} className='fa-2x blog-search-icon' />
 
 const BlogWrapper = () => {
   return (
     <main className='blog-wrapper'>
       <BlogForm />
+      
+      <form className='blog-search-bar-form'>
+        <div className='blog-search-bar-wrapper'>
+          <input 
+            type='text'
+            className='blog-search-bar'
+          ></input>
+
+          <button
+            type='submit'
+            className='blog-search-icon-btn'>
+              {searchIcon}
+            </button>
+        </div>
+      </form>
+
       <BlogPost />
     </main>
   );
