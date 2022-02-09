2be27f41791aa12bdcc107af31e35db661bfb6d7
elliott dombrowski <elliottroyal@me.com>
2022-02-08 23:14:11 -0600

    built starter render for blog form

---
+++ client/src/components/BlogForm/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+import './styles.css';
+import './query.css';
+
+const BlogForm = () => {
+  return (
+    <form className='blog-form-wrapper'>
+      
+    </form>
+  );
+};
+
+export default BlogForm;
