27f23953b8f8a9517e8ce0a262a7cc2507b7fa5a
elliott dombrowski <elliottroyal@me.com>
2022-02-09 16:54:47 -0600

    created starter render for charity page and components

---
+++ client/src/components/CharityCard/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './styles.css';
+import './query.css';
+
+const CharityCard = () => {
+  return (
+    <section className="charity-card-wrapper">
+      <h1>charity card</h1>
+    </section>
+  );
+};
+
+export default CharityCard;
---
+++ client/src/pages/CharityWrapper/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css';
+import './query.css';
+
+const CharityWrapper = () => {
+  return (
+    <main className='charity-wrapper'>
+      <h1>charity homepage</h1>
+    </main>
+  );
+};
+
+export default CharityWrapper;
