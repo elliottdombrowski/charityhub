035f097449404ce46e2f2b21585ef9dc468f4d31
elliott dombrowski <elliottroyal@me.com>
2022-02-08 23:17:00 -0600

    edited navbar to link to blog page

--- client/src/pages/BlogWrapper/index.js
+++ client/src/pages/BlogWrapper/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import BlogForm from '../../components/BlogForm';
 import BlogPost from '../../components/BlogPost';
-import BlogPostBtn from '../../components/BlogPostBtn';
 
 import './styles.css';
 import './query.css';
@@ -9,7 +8,8 @@ import './query.css';
 const BlogWrapper = () => {
   return (
     <main className='blog-wrapper'>
-
+      <BlogForm />
+      <BlogPost />
     </main>
   );
 };
