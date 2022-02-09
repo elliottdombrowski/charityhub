5c0e3751969ba512c2f64a64a03d8848ce84c955
elliott dombrowski <elliottroyal@me.com>
2022-02-09 00:11:42 -0600

    added error styling to character counter

--- client/src/components/BlogForm/index.js
+++ client/src/components/BlogForm/index.js
@@ -1,13 +1,71 @@
 import React, { useState } from 'react';
+import Auth from '../../utils/auth';
 
 import './styles.css';
 import './query.css';
 
 const BlogForm = () => {
+  const [blogPost, setBlogPost] = useState('');
+  const [charCount, setCharCount] = useState(0);
+  const [err, setErr] = useState('');
+
+  const styles = {
+    clear: {
+      color: 'white',
+    },
+    warning: {
+      color: 'yellow',
+    },
+    error: {
+      color: 'red',
+    }
+  }
+  
+  const postWrapperFunction = (event) => {
+    setCharCount(event.target.value.length);
+    setBlogPost(event.target.value);
+  };
+
+  const postSubmit = async (event) => {
+    event.preventDefault();
+
+    setBlogPost('');
+    setCharCount(0);
+    setErr('');
+  };
+
   return (
-    <form className='blog-form-wrapper'>
-      
-    </form>
+    <>
+      {Auth.loggedIn() ? (
+        <form 
+          className='blog-form-wrapper'
+          onSubmit={postSubmit}
+        >
+          <span className='blog-header-wrapper'>
+            <label className='blog-label'>Create a post!</label>
+            <span className='blog-char-count-wrapper'>
+              <p className='blog-char-err'>
+                {err}
+              </p>
+
+              <p 
+                style={charCount >= 500 ? styles.error : charCount >= 450 ? styles.warning : styles.clear}
+                className='blog-char-count'
+              >
+                {charCount} / 500
+              </p>
+            </span>
+          </span>
+          <textarea 
+            className='blog-textarea'
+            value={blogPost}
+            onChange={postWrapperFunction}
+          />
+        </form>
+      ) : (
+       <></> 
+      )}
+    </>
   );
 };
 
