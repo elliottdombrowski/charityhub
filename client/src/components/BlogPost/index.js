9de1449326a543c1b243e4d36c5ecbcf4a96d3f1
elliott dombrowski <elliottroyal@me.com>
2022-02-08 23:15:21 -0600

    added starter render for blog post render

---
+++ client/src/components/BlogPost/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css';
+import './query.css';
+
+const BlogPost = () => {
+  return (
+    <section className='blog-post-wrapper'>
+      
+    </section>
+  );
+};
+
+export default BlogPost;
