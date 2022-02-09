a92773aedf5169165322325c915fe5fa5d52988a
elliott dombrowski <elliottroyal@me.com>
2022-02-09 00:50:19 -0600

    added map rendering of dummy data

--- client/src/components/BlogPost/index.js
+++ client/src/components/BlogPost/index.js
@@ -7,25 +7,25 @@ let dummyPosts = [
   {
     id: 1,
     author: 'dummy one',
-    header: 'post one',
+    title: 'post one',
     body: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
   },
   {
     id: 2,
     author: 'dummy two',
-    header: 'post two',
+    title: 'post two',
     body: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
   },
   {
     id: 3,
     author: 'dummy three',
-    header: 'post three',
+    title: 'post three',
     body: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
   },
   {
     id: 4,
     author: 'dummy four',
-    header: 'post four',
+    title: 'post four',
     body: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
   },
 ];
@@ -33,7 +33,26 @@ let dummyPosts = [
 const BlogPost = () => {
   return (
     <section className='blog-post-wrapper'>
-      
+      {
+        dummyPosts.map((singlePost) => {
+          return (
+            <span className='single-blog-post' key={singlePost.id}>
+              <div className='blog-post-header-wrapper'>
+                <h1 className='blog-post-author'>
+                  {singlePost.author}
+                </h1>
+                <h2 className='blog-post-title'>
+                  {singlePost.title}
+                </h2>
+              </div>
+
+              <p className='blog-post-body'>
+                {singlePost.body}
+              </p>
+            </span>
+          );
+        })
+      };
     </section>
   );
 };
