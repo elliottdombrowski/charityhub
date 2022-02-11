c09edabf86e512cc75d36a73c866eb4f98c3c85d
elliott dombrowski <elliottroyal@me.com>
2022-02-11 15:18:54 -0600

    added state and useeffect to show/hide back to top btn

--- client/src/components/Btns/BackToTopBtn/index.js
+++ client/src/components/Btns/BackToTopBtn/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowCircleUp } from '@fortawesome/free-solid-svg-icons';
@@ -8,6 +8,14 @@ import './styles.scss';
 const backToTop = <FontAwesomeIcon icon={faArrowCircleUp} className='fa-3x back-to-top-icon' />
 
 const BackToTopBtn = () => {
+  const [showBackToTopBtn, setShowBackToTopBtn] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.pageYOffset > 300 ? setShowBackToTopBtn(true) : setShowBackToTopBtn(false);
+    })
+  }, []);
+
   return (
     <button className='back-to-top' id='to-top'>
       <a href='#navbar'>
