97e1de3c625044f429b0f77afd9ca233b9268777
elliott dombrowski <elliottroyal@me.com>
2022-02-13 01:06:18 -0600

    added file structure for emission header component

--- client/src/pages/EmissionDonation/index.js
+++ client/src/pages/EmissionDonation/index.js
@@ -7,17 +7,6 @@ import './query.scss';
 const EmissionDonation = () => {
   return (
     <section className='emission-donation-wrapper'>
-      <div className='emission-donation-text'>
-        <h1 className='emission-donation-header'>
-          Pay your Carbon Emission offsets!
-        </h1>
-        <h2 className='emission-donation-subheader'>
-          Donating to Carbon Emission Offset organizations is a great way to combat your carbon footprint.
-        </h2>
-        <h3 className='emission-donation-desc'>
-          Either pay once, or automatically pay your offsets annually.
-        </h3>
-      </div>
       <span className='emission-donation-cards'>
         <DonationCard />
       </span>
