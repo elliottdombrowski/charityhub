88978fc2e794141d1276374852925abe389a682e
elliott dombrowski <elliottroyal@me.com>
2022-02-16 11:52:19 -0600

    removed dummy array of post objs

--- client/src/components/Cards/BlogPost/index.js
+++ client/src/components/Cards/BlogPost/index.js
@@ -13,48 +13,6 @@ import { QUERY_ALLPOSTS } from '../../../utils/queries';
 import './styles.scss';
 import './query.scss';
 
-let dummyPosts = [
-  {
-    id: 1,
-    author: 'dummy one',
-    title: 'post one',
-    body: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
-  },
-  {
-    id: 2,
-    author: 'dummy two',
-    title: 'post two',
-    body: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
-  },
-  {
-    id: 3,
-    author: 'dummy three',
-    title: 'post three',
-    body: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
-  },
-  {
-    id: 4,
-    author: 'dummy four',
-    title: 'post four',
-    body: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
-  },
-];
-
-// const dummyComments = [
-//   {
-//     post_id: 1,
-//     comment_id: 1,
-//     comment_author: 'commenter one',
-//     comment_body: 'testing for comments 1234 testing',
-//   },
-//   {
-//     post_id: 1,
-//     comment_id: 2,
-//     comment_author: 'commenter two',
-//     comment_body: 'testing for comments 1234 testing',
-//   }
-// ]
-
 const BlogPost = () => {
 
   const { loading, data } = useQuery(QUERY_ALLPOSTS);
