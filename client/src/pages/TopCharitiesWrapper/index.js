6e47af543476e070ed020972e3a19a5b067ad315
elliott dombrowski <elliottroyal@me.com>
2022-02-16 14:46:21 -0600

    setup default render for top charity wrapper in homepage

---
+++ client/src/pages/TopCharitiesWrapper/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.scss';
+import './query.scss';
+
+const TopCharitiesWrapper = () => {
+  return (
+    <section className='top-charity-wrapper'>
+      top charity wrapper
+    </section>
+  );
+};
+
+export default TopCharitiesWrapper;
