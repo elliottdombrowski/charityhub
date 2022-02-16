c6aa523250ff5e3c53e7e6b809bef23a6bfbb754
elliott dombrowski <elliottroyal@me.com>
2022-02-16 14:50:15 -0600

    added default renders for charities header and slider

---
+++ client/src/components/TopCharitiesHeader/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.scss';
+import './query.scss';
+
+const TopCharitiesHeader = () => {
+  return (
+    <span className='top-charities-header-wrapper'>
+      top charities header
+    </span>
+  );
+};
+ 
+export default TopCharitiesHeader;
