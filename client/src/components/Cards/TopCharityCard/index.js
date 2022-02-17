35b1b3e70651f3b24c3e9fe2fc1d0c31112433e0
elliott dombrowski <elliottroyal@me.com>
2022-02-17 09:47:26 -0600

    fixed top charities map function not rendering data

--- client/src/components/Cards/TopCharityCard/index.js
+++ client/src/components/Cards/TopCharityCard/index.js
@@ -3,51 +3,58 @@ import React from 'react';
 import './styles.scss';
 import './query.scss';
 
-let dummyTopCharities = [
+let topCharities = [
   {
-    id: 1,
-    topCharityName: 'charity one',
+    top_id: 1,
+    topCharityName: 'top charity one',
     topCharityDesc: 'lorem ipsum yada yada dummy text',
   },
   {
-    id: 2,
-    topCharityName: 'charity two',
+    top_id: 2,
+    topCharityName: 'top charity two',
     topCharityDesc: 'lorem ipsum yada yada dummy text',
   },
   {
-    id: 3,
-    topCharityName: 'charity three',
+    top_id: 3,
+    topCharityName: 'top charity three',
     topCharityDesc: 'lorem ipsum yada yada dummy text',
   },
   {
-    id: 4,
-    topCharityName: 'charity four',
+    top_id: 4,
+    topCharityName: 'top charity four',
     topCharityDesc: 'lorem ipsum yada yada dummy text',
   },
   {
-    id: 5,
-    topCharityName: 'charity five',
+    top_id: 5,
+    topCharityName: 'top charity five',
     topCharityDesc: 'lorem ipsum yada yada dummy text',
   },
   {
-    id: 6,
-    topCharityName: 'charity six',
+    top_id: 6,
+    topCharityName: 'top charity six',
     topCharityDesc: 'lorem ipsum yada yada dummy text',
   },
 ];
 
 const TopCharityCard = () => {
+  console.log(topCharities)
   return (
     <section className='top-charity-card-wrapper'>
-      <div className='single-top-charity-card'>
-        <div>
-          <h1>one</h1>
-          <h1>two</h1>
-          <div className="top-charity-card-img">
-            <img src={require('./placeholder.jpeg')}></img>
-          </div>
-        </div>
-      </div>
+      {
+        topCharities.map((top) => {
+          return (
+            <span className='single-top-charity-card' key={top.top_id}>
+              <div>
+                <h1>{top.topCharityName}</h1>
+                <h1>{top.topCharityDesc}</h1>
+                <div className="top-charity-card-img">
+                  <img src={require('./placeholder.jpeg')}></img>
+                </div>
+              </div>
+            </span>
+          );
+        })
+      }
     </section>
   );
 };
