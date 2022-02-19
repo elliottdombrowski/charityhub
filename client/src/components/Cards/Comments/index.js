527ffb9180b3e3ad966bcbdfc862e601a9e04e27
elliott dombrowski <elliottroyal@me.com>
2022-02-18 23:18:58 -0600

    added mapping for dummy comment obj data

--- client/src/components/Cards/Comments/index.js
+++ client/src/components/Cards/Comments/index.js
@@ -2,12 +2,57 @@ import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import AnimatePage from '../../../AnimatePage';
 import BackBtn from '../../Btns/BackBtn';
+import ThumbsUpIcon from '../../Icons/ThumbsUpIcon';
+import ThumbsDownIcon from '../../Icons/ThumbsDownIcon';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_SINGLEPOST } from '../../../utils/queries';
 
 import './styles.scss';
 import './query.scss';
+
+const dummyComments = [
+  {
+    id: 1,
+    commentAuthor: 'dummy one',
+    commentBody: 'lorem ipsum dolor sit amet',
+  },
+  {
+    id: 2,
+    commentAuthor: 'dummy two',
+    commentBody: 'lorem ipsum dolor sit amet',
+  },
+  {
+    id: 3,
+    commentAuthor: 'dummy three',
+    commentBody: 'lorem ipsum dolor sit amet',
+  },
+  {
+    id: 4,
+    commentAuthor: 'dummy four',
+    commentBody: 'lorem ipsum dolor sit amet',
+  },
+  {
+    id: 5,
+    commentAuthor: 'dummy five',
+    commentBody: 'lorem ipsum dolor sit amet',
+  },
+  {
+    id: 6,
+    commentAuthor: 'dummy six',
+    commentBody: 'lorem ipsum dolor sit amet',
+  },
+  {
+    id: 7,
+    commentAuthor: 'dummy seven',
+    commentBody: 'lorem ipsum dolor sit amet',
+  },
+  {
+    id: 8,
+    commentAuthor: 'dummy eight',
+    commentBody: 'lorem ipsum dolor sit amet',
+  },
+];
 
 const Comments = () => {
   const { postId } = useParams();
@@ -33,6 +78,19 @@ const Comments = () => {
             {singlePost.body}
           </p>
         </section>
+      </div>
+
+      <div className='single-comment-wrapper'>
+        {
+          dummyComments.map((singleComment) => {
+            return (
+              <div className='single-comment' key={singleComment.id}>
+                <h1>{singleComment.commentAuthor}</h1>
+                <h2>{singleComment.commentBody}</h2>
+              </div>
+            );
+          })
+        }
       </div>
       <BackBtn />
     </AnimatePage>
